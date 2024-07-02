af4a90cd8378aebce7d247ae8b39d6d628ac3cd7
rrinconinnerconsulting <rrincon@innerconsulting.com>
2024-07-02 13:18:06 -0500

    se suben cambios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'swhite-color':'#FFFFFF',
+				'text-color':'#606060',
+				'sblue-general':'#1D70B6',
+				'sblue-dark':'#15497C',
+				'sblue-medium':'#0095DA',
+				'sgreen-spad':'#92BF1F'
+			},
+			backgroundImage:{
+				'spad-linecard': "url('/assets/img/spad_line.jpg')"
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwind-scrollbar'),
+	],
 }
